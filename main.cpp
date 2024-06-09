5486ea6521a9b79aefdc6fcc07e054bc636136fb
Águstin <jose.mendoza.u1@ucb.edu.bo>
2024-06-08 20:42:05 -0400

    Errores de entrada

--- main.cpp
+++ main.cpp
@@ -7,13 +7,13 @@
 using namespace std;
 
 int main() {
-    map<int , vector<string>> BD;
+    map<string , vector<string>> BD;
     int cantidad = 0;
     string dato = "";
     string entrada;
     string comando;
-    string fecha;
-    string evento;
+    string fecha = "";
+    string evento = "";
     string caracter;
     while(true) {
         getline(cin , entrada);
@@ -37,10 +37,98 @@ int main() {
             }
             dato = dato + caracter;
         }
-    cout << comando << endl;
-    cout << fecha << endl;
-    cout << evento << endl;
-
+        dato = "";
+        cantidad = 0;
+        int controlador = 0;
+        string calendario = "";
+        string mes = "";
+        string dia = "";
+        if(comando != "Add" && comando != "Del" && comando != "Find" && comando != "Print") {
+            cout << "Unknow command: " << comando << endl;
+        }
+        for(int i = 0 ; i < fecha.size() ; ++i) {
+            caracter = fecha[i];
+            if(caracter == "-" && i == 0) {
+                continue;
+            }
+            else if (caracter == "-" && controlador == 0) {
+                ++controlador;
+                calendario = dato;
+                dato = "";
+                for(int j = i ; caracter != "1" && caracter != "2" && caracter != "3" && caracter != "4" && caracter != "5" && caracter != "6" && caracter != "7" && caracter != "8" && caracter != "9" && caracter != "0"; ++j) {
+                    caracter = fecha[j];
+                    if(caracter == "-" ) {
+                        ++cantidad;
+                    }
+                }
+                if(cantidad > 2) {
+                    cout << "Wrong date format: " << fecha << endl;
+                    break;
+                }
+                else {
+                    for(int j = i + 1 ; j < fecha.size() ; ++j) {
+                        caracter = fecha[j];
+                        if(caracter == "-" && j == i + 1) {
+                            dato = dato + caracter;
+                            continue;
+                        }
+                        else if(caracter == "-") {
+                            mes = dato;
+                            dato = "";
+                            cantidad = 0;
+                            for(int k = j ; caracter != "1" && caracter != "2" && caracter != "3" && caracter != "4" && caracter != "5" && caracter != "6" && caracter != "7" && caracter != "8" && caracter != "9" && caracter != "0"; ++k) {
+                                caracter = fecha[k];
+                                if(caracter == "-") {
+                                    ++cantidad;
+                                }
+                            }
+                            if(cantidad > 2) {
+                                cout << "Wrong date format: " << fecha << endl;
+                                break;
+                            }
+                            else {
+                                for(int k = j + 1 ; k < fecha.size() ; ++k) {
+                                    caracter = fecha[k];
+                                    if(caracter == "-" && k == j + 1) {
+                                        dato = dato + caracter;
+                                        continue;
+                                    }
+                                    else if(caracter == "-" || k + 1 == fecha.size()) {
+                                        dato = dato + caracter;
+                                        dia = dato;
+                                        break;
+                                    }
+                                    dato = dato + caracter;
+                                }
+                            }
+                            break;
+                        }
+                        else{
+                        dato = dato + caracter;
+                        }
+                    }
+                }
+            }
+            dato = dato + caracter;
+        }
+        cout << calendario << endl;
+        cout << mes << endl;
+        cout << dia << endl;
+        if(calendario == "" || mes == "" || dia == "") {
+            cout << "Wrong date format: " << fecha << endl;
+        }
+        else {
+            int month;
+            int day;
+            month = stoi(mes);
+            day = stoi(dia);
+            if(month < 1 || month > 12) {
+                cout << "Month value is invalid: " << month << endl;
+            }
+            else if(day < 1 || day > 31) {
+                cout << "Day value is invalid: " << day << endl;
+            }
+        }
     }
 
     return 0;
